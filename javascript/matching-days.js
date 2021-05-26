2bd22b26a909a9a56c3bf8f3ded34f8a1aa9d75f
Kim Frans <kimfrans34@gmail.com>
2021-05-26 10:15:10 +0200

    days correspond with colours

--- javascript/matching-days.js
+++ javascript/matching-days.js
@@ -10,229 +10,285 @@ matchingDaysFactory.getDateTwo(dateTwo.value)
 
 dateOne.addEventListener('change', function(){
     matchingDaysFactory.returnDayWeek().weekday1
+    var daysSunday = document.getElementsByClassName(".weekdays")
+    var dayCheckSunday = document.getElementById("day1")
+    var dayCheckMonday = document.getElementById("day2")
+    var dayCheckTuesday = document.getElementById("day3")
+    var dayCheckWednesday = document.getElementById("day4")
+    var dayCheckThursday = document.getElementById("day5")
+    var dayCheckFriday = document.getElementById("day6")
+    var dayCheckSaturday = document.getElementById("day7")
+
+    // dayCheckSunday.classList.remove("daysNoMatch2")
+    dayCheckSunday.classList.remove("daysNoMatch1")
+    // dayCheckSunday.classList.remove("daysMatch")
+
+    // dayCheckMonday.classList.remove("daysNoMatch2")
+    dayCheckMonday.classList.remove("daysNoMatch1")
+    // dayCheckMonday.classList.remove("daysMatch")
+    
+    // dayCheckTuesday.classList.remove("daysNoMatch2")
+    dayCheckTuesday.classList.remove("daysNoMatch1")
+    // dayCheckTuesday.classList.remove("daysMatch")
+
+    // dayCheckWednesday.classList.remove("daysNoMatch2")
+    dayCheckWednesday.classList.remove("daysNoMatch1")
+    // dayCheckWednesday.classList.remove("daysMatch")
+
+    // dayCheckThursday.classList.remove("daysNoMatch2")
+    dayCheckThursday.classList.remove("daysNoMatch1")
+    // dayCheckThursday.classList.remove("daysMatch")
+
+    // dayCheckFriday.classList.remove("daysNoMatch2")
+    dayCheckFriday.classList.remove("daysNoMatch1")
+    // dayCheckFriday.classList.remove("daysMatch")
+
+    // dayCheckSaturday.classList.remove("daysNoMatch2")
+    dayCheckSaturday.classList.remove("daysNoMatch1")
+    // dayCheckSaturday.classList.remove("daysMatch")
+
+
     if(matchingDaysFactory.getDateOne(dateOne.value) == "Sunday"){
         
         if(matchingDaysFactory.checkDays()){
-            var days = document.getElementsByClassName(".weekdays")
-            var dayCheck = document.getElementById("day1")
-            dayCheck.classList.remove("daysNoMatch1")
-            dayCheck.classList.remove("daysNoMatch2")
-            dayCheck.classList.add("daysMatch")
+            
+            dayCheckSunday.classList.remove("daysNoMatch1")
+            dayCheckSunday.classList.remove("daysNoMatch2")
+            dayCheckSunday.classList.add("daysMatch")
             
         }
         else{
-            var days = document.getElementsByClassName(".weekdays")
-            var dayCheck = document.getElementById("day1")
-            dayCheck.classList.remove("daysMatch")
-            dayCheck.classList.add("daysNoMatch1")  
+            dayCheckSunday.classList.remove("daysMatch")
+            dayCheckSunday.classList.add("daysNoMatch1")  
         }
-        
         
     }
     if(matchingDaysFactory.getDateOne(dateOne.value) == "Monday"){
         
         if(matchingDaysFactory.checkDays()){
-            var days = document.getElementsByClassName(".weekdays")
-            var dayCheck = document.getElementById("day2")
-            dayCheck.classList.remove("daysNoMatch1")
-            dayCheck.classList.remove("daysNoMatch2")
-            dayCheck.classList.add("daysMatch")
+            dayCheckMonday.classList.remove("daysNoMatch1")
+            dayCheckMonday.classList.remove("daysNoMatch2")
+            dayCheckMonday.classList.add("daysMatch")
             
         }
         else{
-            var days = document.getElementsByClassName(".weekdays")
-            var dayCheck = document.getElementById("day2")
-            dayCheck.classList.remove("daysMatch")
-            dayCheck.classList.add("daysNoMatch1")  
+            dayCheckMonday.classList.remove("daysMatch")
+            dayCheckMonday.classList.add("daysNoMatch1")  
         }
     
     }
     if(matchingDaysFactory.getDateOne(dateOne.value) == "Tuesday"){
         if(matchingDaysFactory.checkDays()){
-            var days = document.getElementsByClassName(".weekdays")
-            var dayCheck = document.getElementById("day3")
-            dayCheck.classList.remove("daysNoMatch1")
-            dayCheck.classList.remove("daysNoMatch2")
-            dayCheck.classList.add("daysMatch")
+            dayCheckTuesday.classList.remove("daysNoMatch1")
+            dayCheckTuesday.classList.remove("daysNoMatch2")
+            dayCheckTuesday.classList.add("daysMatch")
             
         }
         else{
-            var days = document.getElementsByClassName(".weekdays")
-            var dayCheck = document.getElementById("day3")
-            dayCheck.classList.remove("daysMatch")
-            dayCheck.classList.add("daysNoMatch1")  
+            dayCheckTuesday.classList.remove("daysMatch")
+            dayCheckTuesday.classList.add("daysNoMatch1")  
         }
     }
     if(matchingDaysFactory.getDateOne(dateOne.value) == "Wednesday"){
         if(matchingDaysFactory.checkDays()){
-            var days = document.getElementsByClassName(".weekdays")
-            var dayCheck = document.getElementById("day4")
-            dayCheck.classList.remove("daysNoMatch1")
-            dayCheck.classList.remove("daysNoMatch2")
-            dayCheck.classList.add("daysMatch")
+            dayCheckWednesday.classList.remove("daysNoMatch1")
+            dayCheckWednesday.classList.remove("daysNoMatch2")
+            dayCheckWednesday.classList.add("daysMatch")
         }
         else{
-            var days = document.getElementsByClassName(".weekdays")
-            var dayCheck = document.getElementById("day4")
-            dayCheck.classList.remove("daysMatch")
-            dayCheck.classList.add("daysNoMatch1")  
+            dayCheckWednesday.classList.remove("daysMatch")
+            dayCheckWednesday.classList.add("daysNoMatch1")  
         }
     }
     if(matchingDaysFactory.getDateOne(dateOne.value) == "Thursday"){
         if(matchingDaysFactory.checkDays()){
-            var days = document.getElementsByClassName(".weekdays")
-            var dayCheck = document.getElementById("day5")
-            dayCheck.classList.remove("daysNoMatch1")
-            dayCheck.classList.remove("daysNoMatch2")
-            dayCheck.classList.add("daysMatch")
+            dayCheckThursday.classList.remove("daysNoMatch1")
+            dayCheckThursday.classList.remove("daysNoMatch2")
+            dayCheckThursday.classList.add("daysMatch")
         }
         else{
-            var days = document.getElementsByClassName(".weekdays")
-            var dayCheck = document.getElementById("day5")
-            dayCheck.classList.remove("daysMatch")
-            dayCheck.classList.add("daysNoMatch1")  
+            dayCheckThursday.classList.remove("daysMatch")
+            dayCheckThursday.classList.add("daysNoMatch1")  
         }
     }
     if(matchingDaysFactory.getDateOne(dateOne.value) == "Friday"){
         if(matchingDaysFactory.checkDays()){
-            var days = document.getElementsByClassName(".weekdays")
-            var dayCheck = document.getElementById("day6")
-            dayCheck.classList.remove("daysNoMatch1")
-            dayCheck.classList.remove("daysNoMatch2")
-            dayCheck.classList.add("daysMatch")
+            dayCheckFriday.classList.remove("daysNoMatch1")
+            dayCheckFriday.classList.remove("daysNoMatch2")
+            dayCheckFriday.classList.add("daysMatch")
         }
         else{
-            var days = document.getElementsByClassName(".weekdays")
-            var dayCheck = document.getElementById("day6")
-            dayCheck.classList.remove("daysMatch")
-            dayCheck.classList.add("daysNoMatch1")  
+            dayCheckFriday.classList.remove("daysMatch")
+            dayCheckFriday.classList.add("daysNoMatch1")  
         }
     }
     if(matchingDaysFactory.getDateOne(dateOne.value) == "Saturday"){
         if(matchingDaysFactory.checkDays()){
-            var days = document.getElementsByClassName(".weekdays")
-            var dayCheck = document.getElementById("day7")
-            dayCheck.classList.remove("daysNoMatch1")
-            dayCheck.classList.remove("daysNoMatch2")
-            dayCheck.classList.add("daysMatch")
+            dayCheckSaturday.classList.remove("daysNoMatch1")
+            dayCheckSaturday.classList.remove("daysNoMatch2")
+            dayCheckSaturday.classList.add("daysMatch")
         }
         else{
-            var days = document.getElementsByClassName(".weekdays")
-            var dayCheck = document.getElementById("day7")
-            dayCheck.classList.remove("daysMatch")
-            dayCheck.classList.add("daysNoMatch1")  
+            dayCheckSaturday.classList.remove("daysMatch")
+            dayCheckSaturday.classList.add("daysNoMatch1")  
         }
     }
-
 
 })
 
 dateTwo.addEventListener('change', function(){
     matchingDaysFactory.returnDayWeek().weekday2
+
+    var dayCheckSunday = document.getElementById("day1")
+    var dayCheckMonday = document.getElementById("day2")
+    var dayCheckTuesday = document.getElementById("day3")
+    var dayCheckWednesday = document.getElementById("day4")
+    var dayCheckThursday = document.getElementById("day5")
+    var dayCheckFriday = document.getElementById("day6")
+    var dayCheckSaturday = document.getElementById("day7")
+
+    dayCheckSunday.classList.remove("daysNoMatch2")
+    // dayCheckSunday.classList.remove("daysNoMatch1")
+    // dayCheckSunday.classList.remove("daysMatch")
+
+    dayCheckMonday.classList.remove("daysNoMatch2")
+    // dayCheckMonday.classList.remove("daysNoMatch1")
+    // dayCheckMonday.classList.remove("daysMatch")
+    
+    dayCheckTuesday.classList.remove("daysNoMatch2")
+    // dayCheckTuesday.classList.remove("daysNoMatch1")
+    // dayCheckTuesday.classList.remove("daysMatch")
+
+    dayCheckWednesday.classList.remove("daysNoMatch2")
+    // dayCheckWednesday.classList.remove("daysNoMatch1")
+    // dayCheckWednesday.classList.remove("daysMatch")
+
+    dayCheckThursday.classList.remove("daysNoMatch2")
+    // dayCheckThursday.classList.remove("daysNoMatch1")
+    // dayCheckThursday.classList.remove("daysMatch")
+
+    dayCheckFriday.classList.remove("daysNoMatch2")
+    // dayCheckFriday.classList.remove("daysNoMatch1")
+    // dayCheckFriday.classList.remove("daysMatch")
+
+    dayCheckSaturday.classList.remove("daysNoMatch2")
+    // dayCheckSaturday.classList.remove("daysNoMatch1")
+    // dayCheckSaturday.classList.remove("daysMatch")
+
+
     if(matchingDaysFactory.getDateTwo(dateTwo.value) == "Sunday"){
         
         if(matchingDaysFactory.checkDays()){
-            var days = document.getElementsByClassName(".weekdays")
-            var dayCheck = document.getElementById("day1")
-            dayCheck.classList.remove("daysNoMatch1")
-            dayCheck.classList.add("daysMatch")
+
+            dayCheckSunday.classList.remove("daysNoMatch1")
+            dayCheckSunday.classList.remove("daysNoMatch2")
+            dayCheckSunday.classList.add("daysMatch")
+
+            // dayCheck.classList.remove("daysNoMatch1")
+
+            // dayCheck.classList.add("daysMatch")
         }
         else{
-            var days = document.getElementsByClassName(".weekdays")
-            var dayCheck = document.getElementById("day1")
-            dayCheck.classList.remove("daysMatch")
-            dayCheck.classList.add("daysNoMatch2") 
+            dayCheckSunday.classList.remove("daysMatch")
+            dayCheckSunday.classList.add("daysNoMatch2") 
         }
         
     }
     if(matchingDaysFactory.getDateTwo(dateTwo.value) == "Monday"){
         
         if(matchingDaysFactory.checkDays()){
-            var days = document.getElementsByClassName(".weekdays")
-            var dayCheck = document.getElementById("day2")
-            dayCheck.classList.remove("daysNoMatch1")
-            dayCheck.classList.add("daysMatch")
+
+            dayCheckMonday.classList.remove("daysNoMatch1")
+            dayCheckMonday.classList.remove("daysNoMatch2")
+            dayCheckMonday.classList.add("daysMatch")
+
+            // dayCheck.classList.remove("daysNoMatch1")
+            // dayCheck.classList.add("daysMatch")
         }
         else{
-            var days = document.getElementsByClassName(".weekdays")
-            var dayCheck = document.getElementById("day2")
-            dayCheck.classList.remove("daysMatch")
-            dayCheck.classList.add("daysNoMatch2") 
+            dayCheckMonday.classList.remove("daysMatch")
+            dayCheckMonday.classList.add("daysNoMatch2") 
         }
         
     }
     if(matchingDaysFactory.getDateTwo(dateTwo.value) == "Tuesday"){
         if(matchingDaysFactory.checkDays()){
-            var days = document.getElementsByClassName(".weekdays")
-            var dayCheck = document.getElementById("day3")
-            dayCheck.classList.remove("daysNoMatch1")
-            dayCheck.classList.add("daysMatch")
+            dayCheckTuesday.classList.remove("daysNoMatch1")
+            dayCheckTuesday.classList.remove("daysNoMatch2")
+            dayCheckTuesday.classList.add("daysMatch")
+
+
+            // dayCheck.classList.remove("daysNoMatch1")
+            // dayCheck.classList.add("daysMatch")
         }
         else{
-            var days = document.getElementsByClassName(".weekdays")
-            var dayCheck = document.getElementById("day3")
-            dayCheck.classList.remove("daysMatch")
-            dayCheck.classList.add("daysNoMatch2") 
+            dayCheckTuesday.classList.remove("daysMatch")
+            dayCheckTuesday.classList.add("daysNoMatch2") 
         }
     
     }
     if(matchingDaysFactory.getDateTwo(dateTwo.value) == "Wednesday"){
         if(matchingDaysFactory.checkDays()){
-            var days = document.getElementsByClassName(".weekdays")
-            var dayCheck = document.getElementById("day4")
-            dayCheck.classList.remove("daysNoMatch1")
-            dayCheck.classList.add("daysMatch")
+
+            dayCheckWednesday.classList.remove("daysNoMatch1")
+            dayCheckWednesday.classList.remove("daysNoMatch2")
+            dayCheckWednesday.classList.add("daysMatch")
+            
+            // dayCheck.classList.remove("daysNoMatch1")
+            // dayCheck.classList.add("daysMatch")
         }
         else{
-            var days = document.getElementsByClassName(".weekdays")
-            var dayCheck = document.getElementById("day4")
-            dayCheck.classList.remove("daysMatch")
-            dayCheck.classList.add("daysNoMatch2") 
+            dayCheckWednesday.classList.remove("daysMatch")
+            dayCheckWednesday.classList.add("daysNoMatch2") 
         }
     
     }
     if(matchingDaysFactory.getDateTwo(dateTwo.value) == "Thursday"){
         if(matchingDaysFactory.checkDays()){
-            var days = document.getElementsByClassName(".weekdays")
-            var dayCheck = document.getElementById("day5")
-            dayCheck.classList.remove("daysNoMatch1")
-            dayCheck.classList.add("daysMatch")
+            dayCheckThursday.classList.remove("daysNoMatch1")
+            dayCheckThursday.classList.remove("daysNoMatch2")
+            dayCheckThursday.classList.add("daysMatch")
+
+
+            // dayCheck.classList.remove("daysNoMatch1")
+            // dayCheck.classList.add("daysMatch")
         }
         else{
-            var days = document.getElementsByClassName(".weekdays")
-            var dayCheck = document.getElementById("day5")
-            dayCheck.classList.remove("daysMatch")
-            dayCheck.classList.add("daysNoMatch2") 
+            
+            dayCheckThursday.classList.remove("daysMatch")
+            dayCheckThursday.classList.add("daysNoMatch2") 
         }
     
     }
     if(matchingDaysFactory.getDateTwo(dateTwo.value) == "Friday"){
         if(matchingDaysFactory.checkDays()){
-            var days = document.getElementsByClassName(".weekdays")
-            var dayCheck = document.getElementById("day6")
-            dayCheck.classList.remove("daysNoMatch1")
-            dayCheck.classList.add("daysMatch")
+            dayCheckFriday.classList.remove("daysNoMatch1")
+            dayCheckFriday.classList.remove("daysNoMatch2")
+            dayCheckFriday.classList.add("daysMatch")
+
+            // dayCheck.classList.remove("daysNoMatch1")
+            // dayCheck.classList.add("daysMatch")
         }
         else{
-            var days = document.getElementsByClassName(".weekdays")
-            var dayCheck = document.getElementById("day6")
-            dayCheck.classList.remove("daysMatch")
-            dayCheck.classList.add("daysNoMatch2") 
+            dayCheckFriday.classList.remove("daysMatch")
+            dayCheckFriday.classList.add("daysNoMatch2") 
         }
     
     }
     if(matchingDaysFactory.getDateTwo(dateTwo.value) == "Saturday"){
         if(matchingDaysFactory.checkDays()){
-            var days = document.getElementsByClassName(".weekdays")
-            var dayCheck = document.getElementById("day7")
-            dayCheck.classList.remove("daysNoMatch1")
-            dayCheck.classList.add("daysMatch")
+            dayCheckSaturday.classList.remove("daysNoMatch1")
+            dayCheckSaturday.classList.remove("daysNoMatch2")
+            dayCheckSaturday.classList.add("daysMatch")
+            
+
+            // dayCheck.classList.remove("daysNoMatch1")
+            // dayCheck.classList.add("daysMatch")
         }
         else{
-            var days = document.getElementsByClassName(".weekdays")
-            var dayCheck = document.getElementById("day7")
-            dayCheck.classList.remove("daysMatch")
-            dayCheck.classList.add("daysNoMatch2") 
+            
+            dayCheckSaturday.classList.remove("daysMatch")
+            dayCheckSaturday.classList.add("daysNoMatch2") 
         }
     
     }
